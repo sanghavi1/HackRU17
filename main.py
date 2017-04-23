3dcb15e86b7d6a80c7eefe4c8e81a82b836be4e0
Yash Sanghavi <ysanghavi1@gmail.com>
2017-04-22 20:23:44 -0400

    Tristan is a nigg'er'

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, redirect#, send_from_directory, render_template
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
 from send_sms import sendsms
 app = Flask(__name__, static_folder="/app/static")#, template_folder="/app/templates")
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def main():
         """Respond to incoming calls with a simple text message."""
 	if request.method != 'POST':
@@ -13,14 +15,14 @@ def main():
 	resp = MessagingResponse().message("{}, Thanks for telling me about your day! You said: {}".format(number, message_body))
 	return str(resp)
     
-@app.route("/index")
-def getcss():
-    sendsms()		
-    return send_from_directory("app/static/css", cssFile.css)
+# @app.route("/index")
+# def getcss():
+#     sendsms()		
+#     return send_from_directory("app/static/css", cssFile.css)
 
-@app.route('/')
-def gethtml():
-	return send_from_directory("app/static/html", index.html)
+# @app.route('/')
+# def gethtml():
+# 	return send_from_directory("app/static/html", index.html)
 
 app.run(debug=True, port = 5000)
 
--- send_sms.py
+++ send_sms.py
@@ -2,13 +2,13 @@ from twilio.rest import Client
 
 def sendsms():
     # Your Account SID from twilio.com/console
-    account_sid = "AC386dbcd830dc6c527ba603d502dda72e"
+    account_sid = "ACf4e6e2b01c31b2a04344e6995ea62d87"
     # Your Auth Token from twilio.com/console
-    auth_token  = "ab4a13eebaf65faf6ed08652b3dc633b"
+    auth_token  = "e88828f170b783f25134140d9b3b729b"
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-        to="+12016889444", 
-        from_="+12013545027",
+        to="+17328533909", 
+        from_="+13473219867",
         body="How are you doing today?")
     print(message.sid)
 
